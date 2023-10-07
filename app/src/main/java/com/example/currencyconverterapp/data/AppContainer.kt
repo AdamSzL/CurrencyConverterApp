76e59449794217549033f0b4119a9b3382ae7f9c
AdamSzL <adam@szlosarczyk.com>
2023-10-07 23:02:42 +0200

    Install needed dependencies and configure Retrofit, set up an empty API service

--- app/src/main/java/com/example/currencyconverterapp/data/AppContainer.kt
+++ app/src/main/java/com/example/currencyconverterapp/data/AppContainer.kt
@@ -1,9 +1,25 @@
 package com.example.currencyconverterapp.data
 
+import com.example.currencyconverterapp.network.CurrencyConverterApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 interface AppContainer {
 
 }
 
 class DefaultAppContainer: AppContainer {
+    private val baseUrl =
+        "https://api.currencyapi.com/v3/"
 
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val retrofitService: CurrencyConverterApiService by lazy {
+        retrofit.create(CurrencyConverterApiService::class.java)
+    }
 }
---
+++ app/src/main/java/com/example/currencyconverterapp/network/CurrencyConverterApiService.kt
@@ -0,0 +1,5 @@
+package com.example.currencyconverterapp.network
+
+interface CurrencyConverterApiService {
+
+}
