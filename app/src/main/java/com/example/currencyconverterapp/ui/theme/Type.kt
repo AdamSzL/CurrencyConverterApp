84a5d31fa594bebfcead9cb2a20e9ddb7b541224
AdamSzL <adam@szlosarczyk.com>
2023-10-07 18:38:11 +0200

    Add a custom font (Montserrat)

--- app/src/main/java/com/example/currencyconverterapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.currencyconverterapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.currencyconverterapp.R
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
