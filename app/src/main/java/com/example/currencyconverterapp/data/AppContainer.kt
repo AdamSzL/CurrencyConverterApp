e6753228584570b09d739a09f181e8192ec87617
AdamSzL <adam@szlosarczyk.com>
2023-10-08 21:54:09 +0200

    Add an OkHttp interceptor and a client to insert a header with the api key for each request

--- app/src/main/java/com/example/currencyconverterapp/data/AppContainer.kt
+++ app/src/main/java/com/example/currencyconverterapp/data/AppContainer.kt
@@ -3,7 +3,9 @@ package com.example.currencyconverterapp.data
 import com.example.currencyconverterapp.network.CurrencyConverterApiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 interface AppContainer {
@@ -14,9 +16,22 @@ class DefaultAppContainer: AppContainer {
     private val baseUrl =
         "https://api.currencyapi.com/v3/"
 
+    private val apiKeyInterceptor = Interceptor { chain ->
+        val originalRequest = chain.request()
+        val modifiedRequest = originalRequest.newBuilder()
+            .header("apikey", System.getenv("API_KEY")!!)
+            .build()
+        chain.proceed(modifiedRequest)
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(apiKeyInterceptor)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
+        .client(okHttpClient)
         .build()
 
     private val retrofitService: CurrencyConverterApiService by lazy {
