77fd9390030100ed7e5303c2ca7afd37a52e1994
AdamSzL <adam@szlosarczyk.com>
2024-02-14 02:11:12 +0100

    Generate app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3-android:1.2.0-rc01")
+    implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
