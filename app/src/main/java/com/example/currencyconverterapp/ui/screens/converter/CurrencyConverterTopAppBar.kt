82cc4f4873fc719af37f96a4d9efcdc745966b8a
AdamSzL <adam@szlosarczyk.com>
2024-02-21 01:51:04 +0100

    Change the app bar to be center-aligned, shorten the app bar texts

--- app/src/main/java/com/example/currencyconverterapp/ui/screens/converter/CurrencyConverterTopAppBar.kt
+++ app/src/main/java/com/example/currencyconverterapp/ui/screens/converter/CurrencyConverterTopAppBar.kt
@@ -2,6 +2,7 @@ package com.example.currencyconverterapp.ui.screens.converter
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.currencyconverterapp.ui.CurrencyConverterScreen
+import com.example.currencyconverterapp.ui.theme.Montserrat
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -18,17 +20,12 @@ fun CurrencyConverterTopAppBar(
     currentScreen: CurrencyConverterScreen,
     modifier: Modifier = Modifier
 ) {
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = {
-            Box(
-                modifier = modifier.fillMaxSize()
-            ) {
-                Text(
-                    text = stringResource(currentScreen.title),
-                    style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.align(Alignment.CenterStart),
-                )
-            }
+            Text(
+                text = stringResource(currentScreen.title),
+                style = MaterialTheme.typography.titleLarge.copy(fontFamily = Montserrat),
+            )
         },
         modifier = modifier
     )
